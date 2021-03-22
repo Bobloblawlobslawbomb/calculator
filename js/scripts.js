4c39df0e7751fb107bf940d39c095f25c706a6e4
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-03-22 14:12:27 -0700

    incorporate subtract, multiply, divide and WIP convetTemp functions
    Co-Authored-By: Brandon Magofna <magofna68@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,28 @@
 function add(number1, number2){
   return number1 + number2;
-  }
+}
+
+function subtract(number1, number2){
+  return number1 - number2;
+}
+
+function multiply(number1, number2){
+  return number1 * number2;
+} 
+
+function divide(number1, number2){
+  return number1 / number2;
+}
+
+//function convertTemp() {
+// return (number1 - 32) * 1.8;
+//}
+
+
+
+
+const number1=parseInt(prompt("Enter a number:"));
+const number2=parseInt(prompt("Enter another number:"));
   
-  const number1=parseInt(prompt("Enter a number:"));
-  const number2=parseInt(prompt("Enter another number:"));
-  
-  alert(add(number1, number2));
+alert(add(number1, number2));
+//alert(convertTemp); still working on the temp conversion need a new set of eyes
